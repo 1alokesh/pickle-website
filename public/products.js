0be6dc3fedaca526ecb4ea6446c3e09a176cc750
Panidarapu Lokesh <58964784+1alokesh@users.noreply.github.com>
2026-03-31 23:58:44 +0530

    Create products.js

---
+++ public/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "Guntur Mirchi Pickle",
+    image: "images/Mirchi.jpg",
+    variants: [
+      { size: "250g", price: 79 },
+      { size: "500g", price: 159 },
+      { size: "1kg", price: 299 }
+    ]
+  },
+  {
+    id: 2,
+    name: "Chinta Pandu Pachadi",
+    image: "images/Chinta_Pandu_Pachadi.jpg",
+    variants: [
+      { size: "250g", price: 99 },
+      { size: "500g", price: 199 },
+      { size: "1kg", price: 399 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Chicken Pickle",
+    image: "images/chicken.jpg",
+    variants: [
+      { size: "250g", price: 229 },
+      { size: "500g", price: 449 },
+      { size: "1kg", price: 899 }
+    ]
+  }
+];
