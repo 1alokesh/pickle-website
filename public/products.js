ac84c97e6c2e677e33ec9b27d2d3d16f5771fba6
Panidarapu Lokesh <58964784+1alokesh@users.noreply.github.com>
2026-04-01 08:44:35 +0530

    Update products.js

--- public/products.js
+++ public/products.js
@@ -2,6 +2,7 @@ const products = [
   {
     id: 1,
     name: "Guntur Mirchi Pickle",
+    category: "veg",
     image: "images/Mirchi.jpg",
     variants: [
       { size: "250g", price: 79 },
@@ -12,6 +13,7 @@ const products = [
   {
     id: 2,
     name: "Chinta Pandu Pachadi",
+    category: "veg",
     image: "images/Chinta_Pandu_Pachadi.jpg",
     variants: [
       { size: "250g", price: 99 },
@@ -22,11 +24,23 @@ const products = [
   {
     id: 3,
     name: "Chicken Pickle",
+    category: "nonveg",
     image: "images/chicken.jpg",
     variants: [
       { size: "250g", price: 229 },
       { size: "500g", price: 449 },
       { size: "1kg", price: 899 }
+    ]
+  },
+  {
+    id: 4,
+    name: "Kajjikaya Sweet",
+    category: "snacks",
+    image: "images/Kajjikaya_Sweet.jpg",
+    variants: [
+      { size: "250g", price: 149 },
+      { size: "500g", price: 299 },
+      { size: "1kg", price: 599 }
     ]
   }
 ];
